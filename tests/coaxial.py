157b534aed96a5cd21e6922d351250e0febdeafd
Jakub Kopanski <J.Kopanski@imio.pw.edu.pl>
2014-03-13 10:19:28 +0100

    Changed Equal to AlmostEqual assertions

--- tests/coaxial.py
+++ tests/coaxial.py
@@ -13,7 +13,7 @@ class TestCorrectValues( unittest.TestCase):
                       0     * const.milli, \
                       1.0,                 \
                       1.0)
-        self.assertEqual(50.000, Z0)
+        self.assertAlmostEqual( 50.000, Z0, 1)
     def test_value1( self):
         Z0 = coaxial( 3.038 * const.milli, \
                       7     * const.milli, \
@@ -21,35 +21,35 @@ class TestCorrectValues( unittest.TestCase):
                       1,                   \
                       1)
 
-        self.assertEqual( 50.000, Z0)
+        self.assertAlmostEqual( 50.000, Z0, 1)
     def test_value2( self):
         Z0 = coaxial( 3.026 * const.milli, \
                       7     * const.milli, \
                       0.2   * const.milli, \
                       1,                   \
                       1)
-        self.assertEqual( 50.000, Z0)
+        self.assertAlmostEqual( 50.000, Z0, 1)
     def test_value3( self):
         Z0 = coaxial( 3.040 * const.milli, \
                       7     * const.milli, \
                       0.1   * const.milli, \
                       1,                   \
                       1)
-        self.assertEqual( 49.939, Z0)
+        self.assertAlmostEqual( 49.939, Z0, 1)
     def test_value4( self):
         Z0 = coaxial( 3.040 * const.milli, \
                       7     * const.milli, \
                       0.2   * const.milli, \
                       1,                   \
                       1)
-        self.assertEqual( 49.758, Z0)
+        self.assertAlmostEqual( 49.758, Z0, 1)
     def test_value5( self):
         Z0 = coaxial( 3.040 * const.milli, \
                       7     * const.milli, \
                       0.3   * const.milli, \
                       1,                   \
                       1)
-        self.assertEqual( 49.453, Z0)
+        self.assertAlmostEqual( 49.453, Z0, 1)
 
 if __name__ == '__main__':
     unittest.main()
